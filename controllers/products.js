2181247144b00a6a857bb104080743e36e1b9fa1
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-26 03:08:05 +0200

    adding filtering data method

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,34 @@
 const { errorWrapper } = require("../middleware/erroWrapper");
+const allProductsDB = require("../models/product");
+
 const getAllproducts = errorWrapper(async (request, response) => {
-  throw new Error("zero faute");
+  const { company, featured, name } = request.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? false : true;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  const datafetched = await allProductsDB.find(queryObject);
+
   response.status(200).json({
-    message: "Get All products",
+    status: "success",
+    products: datafetched,
+    totlaClick: datafetched.length,
   });
 });
 
 const getAllProductStatic = errorWrapper(async (request, response) => {
+  const datafetched = await allProductsDB.find({});
   response.status(200).json({
-    message: "Get All products Static",
+    message: datafetched,
   });
 });
 
