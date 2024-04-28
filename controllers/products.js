ac2dd23332cd4266ba10973e268271dbe17ce77c
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-28 15:02:09 +0200

    hotfix

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const { errorWrapper } = require("../middleware/erroWrapper");
 const allProductsDB = require("../models/product");
 
 const getAllproducts = errorWrapper(async (request, response) => {
-  const { company, featured, name } = request.query;
+  const { company, featured, name, sort } = request.query;
   const queryObject = {};
 
   if (featured) {
@@ -16,17 +16,40 @@ const getAllproducts = errorWrapper(async (request, response) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  const datafetched = await allProductsDB.find(queryObject);
+  let datafetched = allProductsDB.find(queryObject);
+
+  if (sort) {
+    console.log(sort);
+    const newSort = sort.split(",").join(" ");
+    datafetched.sort(newSort);
+  } else {
+    datafetched.sort("-createdAt");
+  }
+
+  const myDatafetched = await datafetched;
 
   response.status(200).json({
     status: "success",
-    products: datafetched,
+    products: myDatafetched,
     totlaClick: datafetched.length,
   });
 });
 
 const getAllProductStatic = errorWrapper(async (request, response) => {
-  const datafetched = await allProductsDB.find({}).sort("-price");
+  const { limit } = request.query;
+
+  let datafetched = allProductsDB.find({}).sort("name").select("name price");
+  // if (limit) {
+  //   datafetched = datafetched.limit(Number(limit));
+  // } else {
+  //   datafetched = datafetched;
+  // }
+
+  const page = Number(request.query.page) || 1;
+  const limitation = Number(request.query.limit) || 5;
+  const skip = (page - 1) * limitation;
+
+  datafetched = await datafetched.skip(skip).limit(limitation);
   response.status(200).json({
     message: datafetched,
   });
